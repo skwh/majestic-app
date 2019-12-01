eeb2c26b4be0017cfc7ae0572de6aec93417e2d1
Evan Derby <somekidwithhtml@gmail.com>
2019-11-30 23:54:02 -0700

    Fix google fonts import

--- src/view/App.vue
+++ src/view/App.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=KRoboto+Condensed:700|Roboto:400,700&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:700|Roboto:400,700&display=swap');
 
 html, body {
   margin: 0;
